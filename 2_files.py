779d601d5ea191372fb11216cfce7d4b642ee332
Denniz741 <70800391+Denniz741@users.noreply.github.com>
2020-10-04 20:23:29 +0600

    Update 2_files.py

--- 2_files.py
+++ 2_files.py
@@ -16,7 +16,21 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open('referat.txt', 'r', encoding='utf-8') as refer_2:
+           ref2 = refer_2.read()
+    length = len(ref2)
+    len_word = ref2.replace(',', ' ').replace('.',' ').replace('!',' ').replace(':',' ')\
+        .replace('"', ' ').replace('-'," ").split()
+    quantity_word = len(len_word)
+    referat2 = ref2.replace('.','!')
+   
+    
+    with open('referat2.txt', 'w', encoding='utf-8') as f:
+        f.write(referat2 + '\n  Длина строки равна ' + str(length) + '\n Количество слов равно ' \
+             + str(quantity_word))
+
+
+
 
 if __name__ == "__main__":
     main()
