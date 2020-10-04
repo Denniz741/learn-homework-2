1d3a34b81558957a493a202fb1dfb9a1a699e2f9
Denniz741 <70800391+Denniz741@users.noreply.github.com>
2020-10-04 20:22:16 +0600

    Update 1_date_and_time.py

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -8,21 +8,30 @@
 
 """
 
+from datetime import datetime,timedelta
 def print_days():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-
+    delta = timedelta(days = 1)
+    delta1 = timedelta(days = 30)
+    dt_now = datetime.now()
+    dt_estd = dt_now - delta
+    dt_back30 = dt_now - delta1
+    print(dt_now)
+    print(dt_estd)
+    print(dt_back30)
 
 def str_2_datetime(date_string):
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
 
+    date_string = "01/01/20 12:10:03.234567"
+    datetime.strptime(date_string,'%m/%d/%y %H:%M:%S.%f')
+    
 if __name__ == "__main__":
     print_days()
     print(str_2_datetime("01/01/20 12:10:03.234567"))
